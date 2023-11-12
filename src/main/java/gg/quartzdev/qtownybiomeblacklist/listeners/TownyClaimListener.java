f1f692bb6af61ccdd82bbfae4969d9ce1333a02c
QarthO <qartho@gmail.com>
2023-11-12 05:00:12 -0500

    implemented config

--- src/main/java/gg/quartzdev/qtownybiomeblacklist/listeners/TownyClaimListener.java
+++ src/main/java/gg/quartzdev/qtownybiomeblacklist/listeners/TownyClaimListener.java
@@ -5,44 +5,62 @@ import com.palmergames.bukkit.towny.object.Coord;
 import com.palmergames.bukkit.towny.object.TownBlock;
 import gg.quartzdev.qtownybiomeblacklist.qTownyBiomeBlacklist;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
+import java.util.Collection;
+
 public class TownyClaimListener implements Listener {
 
     qTownyBiomeBlacklist plugin;
+    Collection<Biome> biomeBlacklist;
+    String DENY_MESSAGE;
 
-    public TownyClaimListener(qTownyBiomeBlacklist plugin){
+    public TownyClaimListener(){
         Bukkit.getLogger().info("Registering Towny Events");
-        this.plugin = plugin;
+        this.plugin = qTownyBiomeBlacklist.getInstance();
+        this.biomeBlacklist = plugin.getBiomeBlacklist();
+        this.DENY_MESSAGE = plugin.getDenyMessage();
     }
 
     @EventHandler(priority  = EventPriority.HIGHEST)
     public void onTownyClaim(TownPreClaimEvent event){
-        Bukkit.getLogger().info("TownPreClaimEvent fired");
+
+//        gets the TownBlock
         TownBlock townBlock = event.getTownBlock();
 
-        if(this.getBiomeFromTownBlock(townBlock) == Biome.OCEAN){
-            Bukkit.getLogger().info("Cancelling TownPreClaimEvent because biome is ocean");
-            event.setCancelled(true);
-        }
+//        checks if there is a blacklisted biome
+        Biome biome = containsBlackListedBiome(townBlock);
+        if(biome == null) return;
+
+//        cancels the event and messages the player
+        event.setCancelMessage(this.DENY_MESSAGE.replaceAll("<biome>", biome.name()));
+        event.setCancelled(true);
 
     }
 
-    public Biome getBiomeFromTownBlock(TownBlock townBlock){
+    public Biome containsBlackListedBiome(TownBlock townBlock){
 
+//        gets the world the TownBlock is in
         World world = townBlock.getWorld().getBukkitWorld();
-        Coord coord = townBlock.getCoord();
+        if(world == null)
+            return null;
 
+//        gets the chunk object of the TownBlock
+        Coord coord = townBlock.getCoord();
         int x = coord.getX();
         int z = coord.getZ();
-        int y = 100;
+        Chunk chunk = world.getChunkAt(x, z);
 
-        Location townBlockLocation = new Location(world, x, y, z);
-        return world.getBiome(townBlockLocation);
+//        checks if the chunk contains a blacklisted biome
+        for(Biome biome : this.biomeBlacklist){
+            if(chunk.contains(biome))
+                return biome;
+        }
+        return null;
     }
 }
--- src/main/java/gg/quartzdev/qtownybiomeblacklist/qTownyBiomeBlacklist.java
+++ src/main/java/gg/quartzdev/qtownybiomeblacklist/qTownyBiomeBlacklist.java
@@ -1,15 +1,37 @@
 package gg.quartzdev.qtownybiomeblacklist;
 
 import gg.quartzdev.qtownybiomeblacklist.listeners.TownyClaimListener;
+import org.bukkit.block.Biome;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public final class qTownyBiomeBlacklist extends JavaPlugin {
     private static qTownyBiomeBlacklist instance;
+    private Collection<Biome> biomeBlacklist;
+    private String DENY_MESSAGE;
+
+    public static qTownyBiomeBlacklist getInstance() {
+        return instance;
+    }
+
+    public Collection<Biome> getBiomeBlacklist(){
+        return biomeBlacklist;
+    }
+
+    public String getDenyMessage(){
+        return DENY_MESSAGE;
+    }
 
     @Override
     public void onEnable() {
         instance = this;
-        this.getServer().getPluginManager().registerEvents(new TownyClaimListener(this), this);
+        biomeBlacklist = loadBiomeBlacklist();
+        DENY_MESSAGE = getConfig().getString("deny-msg", "You're not allowed to claim land in <biome>");
+        getServer().getPluginManager().registerEvents(new TownyClaimListener(), instance);
+        saveDefaultConfig();
     }
 
     @Override
@@ -17,7 +39,25 @@ public final class qTownyBiomeBlacklist extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public static qTownyBiomeBlacklist getInstance() {
-        return instance;
+
+    public Collection<Biome> loadBiomeBlacklist(){
+        this.getLogger().info("Loading blacklisted biomes from config...");
+        Collection<Biome> biomeBlacklist = new ArrayList<>();
+        List<?> blacklistedBiomesList = getConfig().getList("blacklisted-biomes");
+        if(blacklistedBiomesList == null) return biomeBlacklist;
+        int blacklistedBiomeCount = 0;
+        for(Object obj : blacklistedBiomesList){
+            String biomeName = (String) obj;
+            try {
+                Biome biome = Biome.valueOf(biomeName);
+                biomeBlacklist.add(biome);
+                blacklistedBiomeCount++;
+                this.getLogger().info("- Added: " + biome.name());
+            } catch(IllegalArgumentException e){
+                this.getLogger().severe("Error: '" + biomeName + "' is not a valid biome");
+            }
+        }
+        this.getLogger().info("Loaded " + blacklistedBiomeCount + " blacklisted biomes");
+        return biomeBlacklist;
     }
 }
